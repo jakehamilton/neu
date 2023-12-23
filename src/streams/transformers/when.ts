a72d7d3ccca52593b6ae24a4498e6b782d55d203
Jake Hamilton <jake.hamilton@hey.com>
2023-12-23 10:23:34 -0800

    feat: when

--- src/streams/transformers/when.ts
+++ src/streams/transformers/when.ts
@@ -1,22 +1,45 @@
-import { Signal, Talkback, Transformer } from "~/streams/interface";
+import { Signal, Source, Talkback, Transformer } from "~/streams/interface";
 
 export const when =
-	<Value>(fn: (value: Value) => boolean): Transformer<Value, Value> =>
+	<Value>(
+		signal: Source<boolean>,
+		initial = false,
+	): Transformer<Value, Value> =>
 	(source) =>
 	(type, sink) => {
 		if (type !== Signal.Start) {
 			return;
 		}
 
-		let talkback: Talkback<unknown> | null = null;
+		let sourceTalkback: Talkback<unknown> | null = null;
+		let signalTalkback: Talkback<unknown> | null = null;
+
+		let isOpen = initial;
+
+		signal(Signal.Start, (type, data) => {
+			if (type === Signal.Start) {
+				signalTalkback = data;
+			} else if (type === Signal.End) {
+				sourceTalkback?.(Signal.End, data);
+
+				sourceTalkback = null;
+				signalTalkback = null;
+			} else {
+				isOpen = data;
+			}
+		});
 
 		source(Signal.Start, (type, data) => {
 			if (type === Signal.Start) {
 				sink(Signal.Start, data);
-				talkback = data;
+				sourceTalkback = data;
 			} else if (type === Signal.End) {
+				signalTalkback?.(Signal.End, data);
 				sink(Signal.End, data);
-			} else if (fn(data)) {
+
+				sourceTalkback = null;
+				signalTalkback = null;
+			} else if (isOpen) {
 				sink(Signal.Data, data);
 			}
 		});
